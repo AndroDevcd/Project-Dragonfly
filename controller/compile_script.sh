960cf47e5b989f1f0cb78cabc9ed0e935e2eb01a
AndroDevcd <braxtonnunnally@gmail.com>
2023-07-31 22:38:59 -0500

    Updated c++ code

--- controller/compile_script.sh
+++ controller/compile_script.sh
@@ -7,6 +7,6 @@ cd ../
 echo "compiling controller c++ code..."
 g++ -DCONTROLLER -o controller/dragonfly.so -shared controller/src/cpp/ui/driver/*.cpp common/src/cpp/gpio/*.cpp common/src/cpp/network/driver/*.cpp controller/generated/*.cpp -lwiringPi -lrf24-bcm
 
-cp controller/dragonfly.so bin/
+cp dragonfly.so bin/
 
 echo "done..."
