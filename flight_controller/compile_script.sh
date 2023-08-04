8b14274df46e7ac1a0829034ad0b870c85f17355
AndroDevcd <braxtonnunnally@gmail.com>
2023-08-04 12:43:18 -0500

    Updated c++ code

--- flight_controller/compile_script.sh
+++ flight_controller/compile_script.sh
@@ -1,5 +1,3 @@
 echo "compiling sharp source code"
-sharpc -L common/ -L flight_controller/ -o bin/fc -nd generated
-echo "compiling c++ code"
-g++ generated/*.cpp flight_controller/device/*.cpp common/gpio/*.cpp common/network/driver/*.cpp -std=c++11 -shared -o bin/dragonfly.so -lrt -lstdc++ -lwiringPi -lrf24 -O3 -Ofast -fPIC
+sharpc -p
 echo "done!"
