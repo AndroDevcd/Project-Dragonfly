212c26921301883f649680537d491a849067d1dd
AndroDevcd <braxtonnunnally@gmail.com>
2023-08-04 13:12:30 -0500

    Updated c++ code

--- flight_controller/compile_script.sh
+++ flight_controller/compile_script.sh
@@ -3,7 +3,7 @@ sharpc -p
 cd ../
 
 echo "compiling controller c++ code..."
-g++ -o flight_controller/dragonfly.so -shared common/src/cpp/gpio/*.cpp common/src/cpp/network/driver/*.cpp -lwiringPi -lrf24-bcm
+g++ -o flight_controller/dragonfly.so -shared common/src/cpp/gpio/*.cpp common/src/cpp/network/driver/*.cpp flight_controller/generated/*.cpp -lwiringPi -lrf24-bcm
 
 cp flight_controller/dragonfly.so /bin/
 
